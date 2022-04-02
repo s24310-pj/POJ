d01eeff7dabee3b73e5c30eae152ab9b3233981a
s24310-pj <92208964+s24310-pj@users.noreply.github.com>
2022-04-02 13:02:09 +0200

    Create kwadrat.java

---
+++ kolokwium3/kwadrat.java
@@ -0,0 +1,24 @@
+package kolokwium2;
+
+public class Kwadrat {
+
+	public Kwadrat(double bok1, double bok2) {
+
+	}
+
+	public Kwadrat(double bok1) {
+
+	}
+
+	public Object przekatna() {
+
+		return null;
+	}
+
+	public int przekatna(int i) {
+		
+		return 0;
+	}
+
+
+}
