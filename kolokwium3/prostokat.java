98f1435b8e90ebba55982677d7c9cfa3d836544c
s24310-pj <92208964+s24310-pj@users.noreply.github.com>
2022-04-02 13:01:42 +0200

    Create prostokat.java

---
+++ kolokwium3/prostokat.java
@@ -0,0 +1,24 @@
+package kolokwium2;
+
+
+
+public class Prostokat {
+
+	public Prostokat(double bok1, double bok2) {
+		
+	}
+	
+	public void Kwadrat(double bok1, double bok2) {
+
+	}
+
+	public double przekatna(double d) {		
+		return Math.sqrt((1 * 1) + (2 * 2)) * 2.5;
+	}
+
+	public Object przekatna() {
+	
+		return null;
+	}
+
+}
